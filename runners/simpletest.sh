230ac0b5100a2ba66c26d6fcec6e81f1a73c58a7
Stuart Clark <stu@rtclark.net>
2016-06-22 12:10:04 +1000

    #6: Added initial automagic discovery logic.

--- runners/simpletest.sh
+++ runners/simpletest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Run simpletests.
-php $SIMPLETEST_SCRIPT --php /opt/phpenv/shims/php --verbose --color --concurrency 4 --url http://127.0.0.1:8080 "$CONFIG_SIMPLETEST_GROUP"
+php $SIMPLETEST_SCRIPT --php /opt/phpenv/shims/php --verbose --color --concurrency 4 --url http://127.0.0.1:8080  --module $CONFIG_DRUPAL_PROJECT
