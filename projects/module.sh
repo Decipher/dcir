d8c8f80669a755c9792762d1d1ac010889422855
Stuart Clark <stu@rtclark.net>
2016-07-18 11:06:34 +1000

    #21: Added ‘—no-dependencies’ flag.

--- projects/module.sh
+++ projects/module.sh
@@ -3,8 +3,12 @@
 export DRUPAL_PROJECT_DIR=$DRUPAL_MODULES_DIR
 
 function drupal_project_init() {
-    echo -e "\nEnable and download project dependencies"
-    echo -e "----------------------------------------\n"
-    drush en $CONFIG_DRUPAL_PROJECT -y
-    echo -e "\n"
+    if [ "${DRUPAL_DEPENDENCIES:-true}" = true ]; then
+        echo
+        echo "Enable and download project dependencies"
+        echo "----------------------------------------"
+        echo
+        drush en $CONFIG_DRUPAL_PROJECT -y
+        echo
+    fi
 }
